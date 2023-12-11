49a2e36a11485fc9f01eb62250f60e4a473e823c
zourva <cqhcwt@126.com>
2023-12-11 16:38:36 +0800

    add ca loading support for signer & fix bug

--- cipher/signer_test.go
+++ cipher/signer_test.go
@@ -8,59 +8,96 @@ import (
 	"time"
 )
 
+var defaultCA = &x509.Certificate{
+	NotBefore: time.Now(),
+	NotAfter:  time.Now().AddDate(100, 0, 0),
+	Subject: pkix.Name{
+		Country:            []string{"CN"},
+		Organization:       []string{"xxx Co.,Ltd."},
+		OrganizationalUnit: []string{"www.example.com"},
+		Province:           []string{"Chongqing"},
+		CommonName:         "example CA",
+	},
+	EmailAddresses: []string{"ca@example.com"},
+}
+
+var descs = []*Descriptor{
+	{
+		Name:       "device",
+		EncodeType: EncodeDER,
+		Info: &x509.Certificate{
+			NotBefore: time.Now(),
+			NotAfter:  time.Now().AddDate(10, 0, 0),
+			Subject: pkix.Name{
+				Country:      []string{"CN"},
+				Organization: []string{"xxx technologies co.,ltd."},
+				Province:     []string{"Beijing"},
+				CommonName:   "xxx device",
+			},
+			EmailAddresses: []string{"dev@xxx.com"},
+		},
+	}, {
+		Name:       "server",
+		EncodeType: EncodePEM,
+		Info: &x509.Certificate{
+			NotBefore: time.Now(),
+			NotAfter:  time.Now().AddDate(20, 0, 0),
+			Subject: pkix.Name{
+				Country:      []string{"CN"},
+				Organization: []string{"xxx technologies co.,ltd."},
+				Province:     []string{"Beijing"},
+				CommonName:   "xxx serfver",
+			},
+			EmailAddresses: []string{"dev@xxx.com"},
+			DNSNames:       []string{"www.example.com"},
+		},
+	},
+}
+
 func TestNewSelfSigner(t *testing.T) {
 	s := NewSelfSigner()
 	assert.NotNil(t, s)
 
-	var defaultCA = &x509.Certificate{
-		NotBefore: time.Now(),
-		NotAfter:  time.Now().AddDate(100, 0, 0),
-		Subject: pkix.Name{
-			Country:            []string{"CN"},
-			Organization:       []string{"xxx Co.,Ltd."},
-			OrganizationalUnit: []string{"www.example.com"},
-			Province:           []string{"Chongqing"},
-			CommonName:         "example CA",
-		},
-		EmailAddresses: []string{"ca@example.com"},
-	}
-
 	assert.NoError(t, s.GenerateCA(defaultCA))
+
+	t.Logf("cert: %s\n", s.CACert())
+	t.Logf("key: %s\n", s.CAPrivateKey())
+
 	assert.NoError(t, s.WriteCACertFiles("ca"))
 
-	descs := []*Descriptor{
-		{
-			Name: "device",
-			Info: &x509.Certificate{
-				NotBefore: time.Now(),
-				NotAfter:  time.Now().AddDate(10, 0, 0),
-				Subject: pkix.Name{
-					Country:      []string{"CN"},
-					Organization: []string{"xxx technologies co.,ltd."},
-					Province:     []string{"Beijing"},
-					CommonName:   "xxx device",
-				},
-				EmailAddresses: []string{"dev@xxx.com"},
-			},
-		}, {
-			Name: "server",
-			Info: &x509.Certificate{
-				NotBefore: time.Now(),
-				NotAfter:  time.Now().AddDate(20, 0, 0),
-				Subject: pkix.Name{
-					Country:      []string{"CN"},
-					Organization: []string{"xxx technologies co.,ltd."},
-					Province:     []string{"Beijing"},
-					CommonName:   "xxx serfver",
-				},
-				EmailAddresses: []string{"dev@xxx.com"},
-				DNSNames:       []string{"www.example.com"},
-			},
-		},
-	}
-	err, i := s.CreateSignCertificates(descs)
+	err, i := s.CreateSelfSignedCertificates(descs)
 	assert.NoError(t, err)
 	assert.Equal(t, i, 2)
 
 	assert.NoError(t, s.WriteFiles(descs))
+}
+
+func TestSelfSigner_LoadAndGenerate(t *testing.T) {
+	s := NewSelfSigner()
+	assert.NoError(t, s.LoadCAFromFiles("ca.crt", "ca.key"))
+
+	err, i := s.CreateSelfSignedCertificates(descs)
+	assert.NoError(t, err)
+	assert.Equal(t, i, 2)
+
+	assert.NoError(t, s.WriteFiles(descs))
+}
+
+func TestSelfSigner_CreateSelfSigned(t *testing.T) {
+	s := NewSelfSigner()
+	assert.NotNil(t, s)
+
+	_, _, err := s.CreateSelfSigned(&x509.Certificate{
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().AddDate(20, 0, 0),
+		Subject: pkix.Name{
+			Country:      []string{"CN"},
+			Organization: []string{"xxx technologies co.,ltd."},
+			Province:     []string{"Beijing"},
+			CommonName:   "xxx serfver",
+		},
+		EmailAddresses: []string{"dev@xxx.com"},
+		DNSNames:       []string{"www.example.com"},
+	})
+	assert.Error(t, err)
 }
